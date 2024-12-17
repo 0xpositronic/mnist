10236ab8a6b8df82525a7ca3a131d00f77f6c7e6
0xpositronic <null0x00000@gmail.com>
2024-12-17 18:31:58 +0300

    refactor done

--- mnist.cu
+++ mnist.cu
@@ -5,17 +5,31 @@
 #include <stdio.h>
 #include <time.h>
 
+typedef enum { DENSE, RELU, SOFTMAX } LayerType;
 typedef struct {
+  LayerType type;
   int ins;
   int outs;
   float *W;
   float *H;
 } Layer;
-
 typedef struct {
   Layer *layers;
   int num_layers;
 } Network;
+
+void lilfbig(uint32_t *big) { // big-endian to little-endian
+  *big = (*big >> 24) | (*big << 24) | ((*big & 0x0000FF00) << 8) |
+         ((*big & 0x00FF0000) >> 8);
+}
+
+void save_matrix(float *data, int rows, int cols, const char *filename) {
+  FILE *f = fopen(filename, "wb");
+  fwrite(&rows, sizeof(int), 1, f);
+  fwrite(&cols, sizeof(int), 1, f);
+  fwrite(data, sizeof(float), rows * cols, f);
+  fclose(f);
+}
 
 void print_mat(float *A, int *sizes) {
   for (int i = 0; i < sizes[0]; i++) {
@@ -37,15 +51,6 @@ void matmul(float *A, float *B, float *C, int *sizes) { // less slow
     }
   }
 }
-void matmul_tester() {
-  float A[6] = {1, 2, 3, 4, 5, 6};
-  float B[3] = {-6, -5, -4};
-  float C[2] = {0, 0};
-  int sizes[4] = {2, 3, 3, 1};
-  int out_sizes[2] = {sizes[0], sizes[3]};
-  matmul(A, B, C, sizes);
-  print_mat(C, out_sizes);
-}
 
 float *transpose(float *A, int *sizes) {
   float *At = (float *)malloc(sizeof(float) * sizes[0] * sizes[1]);
@@ -56,73 +61,14 @@ float *transpose(float *A, int *sizes) {
   }
   return At;
 }
-void transpose_tester() {
-  float A[6] = {1, 2, 3, 4, 5, 6};
-  int sizes[2] = {2, 3};
-  float *At = transpose(A, sizes);
-  int sizest[2] = {sizes[1], sizes[0]};
-  print_mat(A, sizes);
-  print_mat(At, sizest);
-}
 
-float *relu(float *A, int size) {
-  float *Ar = (float *)malloc(sizeof(float) * size);
-  for (int i = 0; i < size; i++) {
-    if (A[i] < 0) {
-      Ar[i] = 0;
-    } else
-      Ar[i] = A[i];
-  }
-  return Ar;
-}
-
-float *softmax(float *A, int size) {
-  float *As = (float *)malloc(sizeof(float) * size);
-  float sum = 0.0;
-  for (int i = 0; i < size; i++) {
-    sum += exp(A[i]);
-  }
-  for (int i = 0; i < size; i++) {
-    As[i] = exp(A[i]) / sum;
-  }
-  return As;
-}
-
-float *zeros(int size) {
-  float *A = (float *)malloc(sizeof(float) * size);
-  for (int i = 0; i < size; i++) {
-    A[i] = 0;
-  }
-  return A;
-}
-float *fill(int size) {
-  float *A = (float *)malloc(sizeof(float) * size);
-  for (int i = 0; i < size; i++) {
-    A[i] = i;
-  }
-  return A;
-}
-
-void lilfbig(uint32_t *big) { // big-endian to little-endian
-  *big = (*big >> 24) | (*big << 24) | ((*big & 0x0000FF00) << 8) |
-         ((*big & 0x00FF0000) >> 8);
-}
-
-void save_matrix(float *data, int rows, int cols, const char *filename) {
-  FILE *f = fopen(filename, "wb");
-  fwrite(&rows, sizeof(int), 1, f);
-  fwrite(&cols, sizeof(int), 1, f);
-  fwrite(data, sizeof(float), rows * cols, f);
-  fclose(f);
-}
-
-float *xavier_init(int fan_in, int fan_out) {
-  float *weights = (float *)malloc(sizeof(float) * fan_in * fan_out);
-  float scale = sqrtf(2.0f / (fan_in + fan_out));
+float *xavier_init(int ins, int outs) {
+  float *weights = (float *)malloc(sizeof(float) * ins * outs);
+  float scale = sqrtf(2.0f / (ins + outs));
 
   srand(1);
 
-  for (int i = 0; i < fan_in * fan_out; i++) {
+  for (int i = 0; i < ins * outs; i++) {
     // Generate random number between -1 and 1
     float rand_val = ((float)rand() / RAND_MAX) * 2 - 1;
     weights[i] = rand_val * scale;
@@ -130,23 +76,108 @@ float *xavier_init(int fan_in, int fan_out) {
   return weights;
 }
 
-float *forward(float *weights1, float *weights2, float *weights3, float *out1,
-               float *out2, float *out3, int *layer_sizes1, int *layer_sizes2,
-               int *layer_sizes3, int *data_size, float *train_data_T) {
-  float *h1, *h2, *h3;
-  matmul(weights1, train_data_T, out1, layer_sizes1);
-  h1 = relu(out1, 128 * data_size[0]);
-  matmul(weights2, h1, out2, layer_sizes2);
-  h2 = relu(out2, 64 * data_size[0]);
-  matmul(weights3, h2, out3, layer_sizes3);
-  h3 = softmax(out3, 10 * data_size[0]);
-  return h3;
+Layer create_dense(int ins, int outs) {
+  Layer layer;
+  layer.type = DENSE;
+  layer.ins = ins;
+  layer.outs = outs;
+  layer.W = xavier_init(outs, ins);
+  layer.H = NULL; // cant know the batch size here
+  return layer;
+}
+Layer create_relu() {
+  Layer layer;
+  layer.type = RELU;
+  layer.ins = 0;
+  layer.outs = 0;
+  layer.W = NULL;
+  layer.H = NULL;
+  return layer;
+}
+Layer create_softmax() {
+  Layer layer;
+  layer.type = SOFTMAX;
+  layer.ins = 0;
+  layer.outs = 0;
+  layer.W = NULL;
+  layer.H = NULL;
+  return layer;
+}
+
+void free_layer(Layer *layer) {
+  if (layer->W != NULL)
+    free(layer->W);
+  if (layer->H != NULL)
+    free(layer->H);
+}
+
+float *layer_forward(Layer *layer, float *input, int input_size) {
+  if (layer->H != NULL)
+    free(layer->H); // free previous output since we will create a new one, we
+                    // would lose the previous.
+
+  switch (layer->type) {
+  case DENSE: {
+    layer->H = (float *)malloc(
+        sizeof(float) * layer->ins *
+        layer->outs); // no need to init to 0 since we use mid sum
+
+    int sizes[4] = {layer->outs, layer->ins, layer->ins, input_size};
+    matmul(layer->W, input, layer->H, sizes);
+    break;
+  }
+  case RELU: {
+    layer->ins = input_size;
+    layer->outs = input_size;
+    layer->H = (float *)malloc(sizeof(float) * input_size);
+    for (int i = 0; i < input_size; i++) {
+      layer->H[i] = input[i] > 0 ? input[i] : 0;
+    }
+    break;
+  }
+  case SOFTMAX: {
+    layer->ins = input_size;
+    layer->outs = input_size;
+    layer->H = (float *)malloc(sizeof(float) * input_size);
+    float sum = 0.0f;
+    for (int i = 0; i < input_size; i++) {
+      sum += exp(input[i]);
+    }
+    for (int i = 0; i < input_size; i++) {
+      layer->H[i] = exp(input[i]) / sum;
+    }
+    break;
+  }
+  }
+  return layer->H;
+}
+
+Network create_network() {
+  Network net;
+  net.layers = NULL;
+  net.num_layers = 0;
+  return net;
+}
+void free_network(Network *net) {
+  for (int i = 0; i < net->num_layers; i++) {
+    free_layer(&net->layers[i]);
+  }
+  free(net->layers);
+}
+void add_layer(Network *net, Layer layer) {
+  net->num_layers++;
+  net->layers = (Layer *)realloc(net->layers, sizeof(Layer) * net->num_layers);
+  net->layers[net->num_layers - 1] = layer;
+}
+float *forward(Network *net, float *input, int input_size) {
+  float *current_input = input;
+  for (int i = 0; i < net->num_layers; i++) {
+    current_input = layer_forward(&net->layers[i], current_input, input_size);
+  }
+  return current_input;
 }
 
 int main() {
-  matmul_tester();
-  transpose_tester();
-
   FILE *train_images = fopen(DATA_DIR "train-images-idx3-ubyte", "rb");
   FILE *train_labels = fopen(DATA_DIR "train-labels-idx1-ubyte", "rb");
   FILE *test_images = fopen(DATA_DIR "t10k-images-idx3-ubyte", "rb");
@@ -176,14 +207,14 @@ int main() {
   printf("data type: %d\n", data_type);
   printf("dimensions: %d\n", dimension);
 
-  int train_size[dimension];
+  int data_size[dimension];
   uint32_t size;
   size_t total_size = 1;
   for (int i = 0; i < dimension; i++) {
     fread(&size, sizeof(uint32_t), 1, train_images);
     lilfbig(&size);
     total_size *= size;
-    train_size[i] = (int)size;
+    data_size[i] = (int)size;
     printf("%d\n", size);
   }
   printf("total data size: %lu\n", total_size);
@@ -195,29 +226,25 @@ int main() {
     train_data[i] = ((float)pixel / 127.5f) - 1.0f;
   }
 
-  float *weights1 = xavier_init(128, 28 * 28);
-  float *weights2 = xavier_init(64, 128);
-  float *weights3 = xavier_init(10, 64);
+  int train_size[2] = {data_size[0], data_size[1] * data_size[2]};
+  float *input;
+  input = transpose(train_data, train_size);
 
-  float *out1 = zeros(train_size[0] * 128);
-  float *out2 = zeros(train_size[0] * 64);
-  float *out3 = zeros(train_size[0] * 10);
+  Network net = create_network();
+  add_layer(&net, create_dense(train_size[1], 128));
+  add_layer(&net, create_relu());
+  add_layer(&net, create_dense(128, 64));
+  add_layer(&net, create_relu());
+  add_layer(&net, create_dense(64, 10));
+  add_layer(&net, create_softmax());
 
-  int data_size[2] = {train_size[0], train_size[1] * train_size[2]};
-  float *train_data_T = transpose(train_data, data_size);
-
-  int layer_sizes1[4] = {128, 28 * 28, data_size[1], data_size[0]};
-  int layer_sizes2[4] = {64, 128, 128, data_size[0]};
-  int layer_sizes3[4] = {10, 64, 64, data_size[0]};
-
-  for (int iter = 0; iter < 10; iter++) {
-    float *output =
-        forward(weights1, weights2, weights3, out1, out2, out3, layer_sizes1,
-                layer_sizes2, layer_sizes3, data_size, train_data_T);
-    // calculate loss. loss()
-    // update weights. backward()
+  int iters = 10;
+  for (int i = 0; i < iters; i++) {
+    input = forward(&net, input, data_size[0]);
+    // loss = calc_loss(input, labels);
+    // net = backward(&net, ...);
   }
-  // calculate final loss and accuracy
-  // save weights
+
+  free_network(&net);
   return 0;
 }
