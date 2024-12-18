7776bc82707781b923eccb44ae1c11518397527d
0xpositronic <null0x00000@gmail.com>
2024-12-18 18:50:37 +0300

    implementing_bacward()

--- mnist.cu
+++ mnist.cu
@@ -12,6 +12,8 @@ typedef struct {
   int outs;
   float *W;
   float *H;
+  float *dW;
+  float *dH;
 } Layer;
 typedef struct {
   Layer *layers;
@@ -83,24 +85,30 @@ Layer create_dense(int ins, int outs) {
   layer.outs = outs;
   layer.W = xavier_init(outs, ins);
   layer.H = NULL; // cant know the batch size here
+  layer.dW = NULL;
+  layer.dH = NULL;
   return layer;
 }
-Layer create_relu() {
+Layer create_relu(int size) {
   Layer layer;
   layer.type = RELU;
-  layer.ins = 0;
-  layer.outs = 0;
+  layer.ins = size;
+  layer.outs = size;
   layer.W = NULL;
   layer.H = NULL;
+  layer.dW = NULL;
+  layer.dH = NULL;
   return layer;
 }
-Layer create_softmax() {
+Layer create_softmax(int size) {
   Layer layer;
   layer.type = SOFTMAX;
-  layer.ins = 0;
-  layer.outs = 0;
+  layer.ins = size;
+  layer.outs = size;
   layer.W = NULL;
   layer.H = NULL;
+  layer.dW = NULL;
+  layer.dH = NULL;
   return layer;
 }
 
@@ -127,24 +135,34 @@ float *layer_forward(Layer *layer, float *input, int input_size) {
     break;
   }
   case RELU: {
-    layer->ins = input_size;
-    layer->outs = input_size;
-    layer->H = (float *)malloc(sizeof(float) * input_size);
-    for (int i = 0; i < input_size; i++) {
+    layer->H = (float *)malloc(sizeof(float) * input_size * layer->ins);
+    for (int i = 0; i < input_size * layer->ins; i++) {
       layer->H[i] = input[i] > 0 ? input[i] : 0;
     }
     break;
   }
   case SOFTMAX: {
-    layer->ins = input_size;
-    layer->outs = input_size;
-    layer->H = (float *)malloc(sizeof(float) * input_size);
-    float sum = 0.0f;
-    for (int i = 0; i < input_size; i++) {
-      sum += exp(input[i]);
-    }
-    for (int i = 0; i < input_size; i++) {
-      layer->H[i] = exp(input[i]) / sum;
+    layer->H = (float *)malloc(sizeof(float) * input_size * layer->ins);
+    for (int i = 0; i < input_size; i++) { // for each sample
+      // Find max for numerical stability
+      float max_val = input[0 * input_size + i];
+      for (int j = 1; j < layer->ins; j++) {
+        if (input[j * input_size + i] > max_val) {
+          max_val = input[j * input_size + i];
+        }
+      }
+
+      // Compute sum of exp(x - max_val)
+      float sum = 0.0f;
+      for (int j = 0; j < layer->ins; j++) {
+        sum += exp(input[j * input_size + i] - max_val);
+      }
+
+      // Compute softmax
+      for (int j = 0; j < layer->ins; j++) {
+        layer->H[j * input_size + i] =
+            exp(input[j * input_size + i] - max_val) / sum;
+      }
     }
     break;
   }
@@ -172,12 +190,81 @@ void add_layer(Network *net, Layer layer) {
 float *forward(Network *net, float *input, int input_size) {
   float *current_input = input;
   for (int i = 0; i < net->num_layers; i++) {
-    printf("Layer %d forward pass (type=%d, ins=%d, outs=%d, input_size=%d)\n",
-           i, net->layers[i].type, net->layers[i].ins, net->layers[i].outs,
-           input_size);
     current_input = layer_forward(&net->layers[i], current_input, input_size);
   }
   return current_input;
+}
+
+float *layer_backward(Layer *layer, float *dH_above, int *size_above) {
+  if (layer->dW != NULL)
+    free(layer->dW);
+
+  switch (layer->type) {
+  case DENSE: {
+    break;
+  }
+  case RELU: {
+    break;
+  }
+  case SOFTMAX: {
+    for (int i = 0; i < size_above[1]; i++) {
+      for (int j = 0; j < size_above[0]; j++) {
+        layer->dH[j * size_above[1] + i] = -1 * (-);
+      }
+    }
+    break;
+  }
+  }
+  return layer->dH;
+}
+
+void backward(Network *net, float loss, int *sizes, int *y) {
+  float *previous_output = net->layers[net->num_layers - 1].H;
+  float *grad = (float *)malloc(sizeof(float) * sizes[0] * sizes[1]);
+
+  for (int i = 0; i < sizes[1]; i++) {
+    for (int j = 0; j < sizes[0]; j++) {
+      if (j == y[i]) {
+        grad[j * sizes[1] + i] =
+            (-1.0f / previous_output[j * sizes[1] + i]) / sizes[1];
+      } else {
+        grad[j * sizes[1] + i] = 0.0f;
+      }
+    }
+  }
+  for (int i = net->num_layers; i > 0; i--) {
+    int size_above[2] = {1, 1};
+    layer_backward(&net->layers[i - 1], grad, size_above);
+  }
+}
+
+int *get_prediction(float *h, int *sizes) {
+  int *predictions = (int *)malloc(sizeof(int) * sizes[0] * sizes[1]);
+  for (int i = 0; i < sizes[1]; i++) {
+    float pred = 0;
+    int max;
+    for (int j = 0; j < sizes[0]; j++) {
+      if (h[j * sizes[1] + i] > pred) {
+        pred = h[j * sizes[1] + i];
+        max = j;
+      }
+    }
+    predictions[i] = max;
+  }
+  return predictions;
+}
+
+float calc_loss(float *h, int *y, int *sizes) {
+  float loss = 0.0f;
+  const float epsilon = 1e-7f;
+
+  for (int i = 0; i < sizes[1]; i++) {
+    float true_class_prob = h[y[i] * sizes[1] + i];
+    true_class_prob = fmaxf(true_class_prob, epsilon); // return max of two
+    loss -= logf(true_class_prob);
+  }
+  loss /= sizes[1];
+  return loss;
 }
 
 int main() {
@@ -189,10 +276,9 @@ int main() {
   // read first 32 bits / 4 bytes
   uint32_t magic_number;
   fread(&magic_number, sizeof(uint32_t), 1, train_images);
-
   // big endian: first byte of the 4 byte magic number is on the right
   // | 0x03 | 0x08 | 00000000 | *00000000* |
-  printf("magic number: %d\n", magic_number);
+  // printf("magic number: %d\n", magic_number);
   // 50855936 = 11000010000000000000000000
   /*
   Little endian
@@ -203,7 +289,6 @@ int main() {
   Address	0	1	2	3
   Data	03	08	00	00
   */
-
   // get the 3rd and 4th bytes. 0xFF is 11111111, a full byte.
   uint8_t data_type = (magic_number >> 16) & 0xFF;
   uint8_t dimension = (magic_number >> 24) & 0xFF;
@@ -228,6 +313,17 @@ int main() {
     fread(&pixel, sizeof(uint8_t), 1, train_images);
     train_data[i] = ((float)pixel / 127.5f) - 1.0f;
   }
+  fclose(train_images);
+
+  fseek(train_labels, 8, SEEK_SET);
+  int *train_classes = (int *)malloc(sizeof(int) * 60000);
+  uint8_t label;
+  int la = 0;
+  for (int i = 0; i < 60000; i++) {
+    fread(&label, 1, 1, train_labels);
+    train_classes[i] = (int)label;
+  }
+  fclose(train_labels);
 
   int train_size[2] = {data_size[0], data_size[1] * data_size[2]};
   float *input;
@@ -235,18 +331,19 @@ int main() {
 
   Network net = create_network();
   add_layer(&net, create_dense(train_size[1], 128));
-  add_layer(&net, create_relu());
+  add_layer(&net, create_relu(128));
   add_layer(&net, create_dense(128, 64));
-  add_layer(&net, create_relu());
+  add_layer(&net, create_relu(64));
   add_layer(&net, create_dense(64, 10));
-  add_layer(&net, create_softmax());
+  add_layer(&net, create_softmax(10));
 
-  int iters = 10;
-  float *h;
+  int out_size[2] = {10, train_size[0]};
+  int iters = 2;
   for (int i = 0; i < iters; i++) {
-    h = forward(&net, input, data_size[0]);
-    // loss = calc_loss(input, labels);
-    // net = backward(&net, ...);
+    float *h = forward(&net, input, data_size[0]);
+    float loss = calc_loss(h, train_classes, out_size);
+    printf("iter=%d -- loss=%.4f", i, loss);
+    backward(&net, loss, out_size);
   }
 
   free_network(&net);
--- readme.md
+++ readme.md
@@ -1,1 +1,11 @@
 Mnist classifier in C/CUDA
+
+current stage: C implementation
+- matmul and dense layer implemented and tested
+- full forward pass implemented, not tested
+
+todo:
+backprop
+test against numpy/pytorch
+visualizations
+CUDA implementation and testing
